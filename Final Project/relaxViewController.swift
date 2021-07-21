e930d55d03ca10987e95c66fbf3d4eefb7dea5bd
Rakshaa Venkatraman <rakshaavenkatraman@gmail.com>
2021-07-21 13:42:21 -0700

    added files for all the view controllers

---
+++ Final Project/relaxViewController.swift
@@ -0,0 +1,29 @@
+//
+//  relaxViewController.swift
+//  Final Project
+//
+//  Created by Rakshaa Venkatraman on 7/21/21.
+//
+
+import UIKit
+
+class relaxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
