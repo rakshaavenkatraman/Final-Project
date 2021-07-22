82e37ee6de9ab93e3be358cbb3d6472ba72e7f6c
Rakshaa Venkatraman <rakshaavenkatraman@gmail.com>
2021-07-22 12:14:22 -0700

    fixed links

--- focusViewController.swift
+++ focusViewController.swift
@@ -15,12 +15,12 @@ class focusViewController: UIViewController {
     }
     
     @IBAction func studyMusicButton(_ sender: Any) {
-        let studyMusic = URL (string:"https://www.youtube.com/watch?v=sjkrrmBnpGE")
+        let studyMusic = URL (string:"https://open.spotify.com/search/study%20music")
         UIApplication.shared.open(studyMusic!)
     }
     
     @IBAction func timeManagePomodoro(_ sender: Any) {
-        let timeManage = URL (string: "https://www.youtube.com/watch?v=1Cv0kCB59J4")
+        let timeManage = URL (string: "https://pomofocus.io/")
         UIApplication.shared.open(timeManage!)
     }
     
